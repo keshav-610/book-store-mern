75e80ffe421ae42196860950d72ef3465d4bf703
keshav-610 <kesavaprakash1610@gmail.com>
2024-12-01 11:39:26 +0530

    route update check

--- backend/index.js
+++ backend/index.js
@@ -1,39 +1,42 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const app = express();
-require("dotenv").config();
 const cors = require("cors");
 const path = require("path");
+require("dotenv").config();
 
+const app = express();
+
+// Middleware
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://book-store-mern-frontend-theta.vercel.app"],
+    origin: ["http://localhost:5173", "https://book-store-mern-frontend-theta.vercel.app"],
     credentials: true,
   })
 );
 
+// Routes
 const bookRoutes = require("./src/books/book_route");
 const orderRoutes = require("./src/orders/order_route");
 const userRoutes = require("./src/users/user_routes");
-const adminRoutes = require("./src/stats/admin_stats")
-
+const adminRoutes = require("./src/stats/admin_stats");
 
 app.use("/assets/books", express.static(path.join(__dirname, "../public/books")));
 app.use("/api/books", bookRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/auth", userRoutes);
-app.use("/api/admin",adminRoutes)
+app.use("/api/admin", adminRoutes);
 
+// Connect to MongoDB
 mongoose
   .connect(process.env.DB_URL)
   .then(() => console.log("DB connection established"))
   .catch((err) => console.log(err));
 
+// Root route
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running at http://localhost:${process.env.PORT}`);
-});
+// Vercel requires serverless export
+module.exports = app;
